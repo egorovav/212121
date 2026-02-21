95d796a204e2c6a6ac1967fea1c07ae7308c180c
egorovav <egorov_av@mail.ru>
2026-02-21 13:23:20 +0300

    Update game_of_life.c

--- src/game_of_life.c
+++ src/game_of_life.c
@@ -1,37 +1,39 @@
-#include <stdio.h>
-#include <ncurses.h>
-#include "adapters/messages.h"
-#include "adapters/keyboard_actions.h"
 #include "adapters/data_io.h"
+#include "adapters/keyboard_actions.h"
+#include "adapters/messages.h"
 #include "usecases/game_logic.h"
+#include <ncurses.h>
+#include <stdio.h>
 
 int changeStdin() {
-    if (freopen("/dev/tty", "r", stdin) == NULL) {
-        //printError();
-        return -1;
-    }
+  if (freopen("/dev/tty", "r", stdin) == NULL) {
+    // printError();
+    return -1;
+  }
 
-    return 0;
+  return 0;
 }
 
-
 int main() {
-    int current_data[ROWS][COLLS];
-    if (input(current_data) == -1) {
-        //printError();
-        return -1;
-    }
+  int current_data[ROWS][COLLS];
+  int result = 0;
+  if (input(current_data) == -1) {
+    result = -1;
+  }
 
-    FILE *tty = fopen("/dev/tty", "r+");
-    if (tty == NULL) {
-        return -1;
-    }
+  FILE *tty = fopen("/dev/tty", "r+");
+  if (tty == NULL) {
+    result = -1;
+  }
 
-    SCREEN *term = newterm(NULL, tty, tty);
-    set_term(term);
+  SCREEN *term = newterm(NULL, tty, tty);
+  set_term(term);
 
-    cbreak();
-    noecho();
+  cbreak();
+  noecho();
+  if (result) {
+    printError("Input data error.");
+  } else {
     printHello();
     refresh();
     nodelay(stdscr, TRUE);
@@ -41,24 +43,26 @@ int main() {
     int speed = 100;
 
     while ((key_pressed = getch()) && !checkStop(key_pressed)) {
-        if (checkPause(key_pressed)) {
-            is_pause = !is_pause;
-        }
+      if (checkPause(key_pressed)) {
+        is_pause = !is_pause;
+      }
 
-        speedChange(key_pressed, &speed);
-        if (!is_pause) {
-            int new_data[ROWS][COLLS];
-            fillNewGeneration(new_data, current_data);
-            copy(current_data, new_data);
-        }
-        clear();
-        output(current_data);
-        mvprintw(ROWS, 0, "Delay: %d ms | Pause: %s", speed, is_pause ? "ON" : "OFF");
-        refresh();
+      speedChange(key_pressed, &speed);
+      if (!is_pause) {
+        int new_data[ROWS][COLLS];
+        fillNewGeneration(new_data, current_data);
+        copy(current_data, new_data);
+      }
+      clear();
+      output(current_data);
+      mvprintw(ROWS, 0, "Delay: %d ms | Pause: %s", speed,
+               is_pause ? "ON" : "OFF");
+      refresh();
 
-        napms(speed);
+      napms(speed);
     }
+  }
 
-    endwin();
-    return 0;
+  endwin();
+  return result;
 }
