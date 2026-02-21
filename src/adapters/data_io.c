599b364f96871a0177efe4048ec2880974cf8466
egorovav <egorov_av@mail.ru>
2026-02-21 13:24:19 +0300

    Update data_io.c

--- src/adapters/data_io.c
+++ src/adapters/data_io.c
@@ -6,10 +6,19 @@ int input(int data[ROWS][COLLS]) {
     for (int i = 0; i < ROWS; i++) {
         for (int j = 0; j < COLLS; j++) {
             char c;
-            if (scanf("%d%c", &data[i][j], &c) != 1 && c != '\n' && c != ' ' && c != EOF) {
+            if (scanf("%d%c", &data[i][j], &c) != 2) {
                 return -1;
             }
-        }
+            if(data[i][j] != 0 && data[i][j] != 1) {
+            	return -1;
+            }
+            if(c != '\n' && c != ' ' && c != EOF && c != '\r') {
+            	return -1;
+            }
+            if(j == COLLS - 1 && c != '\n' && c != '\r') {
+            	return -1;
+            }
+    	}
     }
     return 0;
 }
@@ -25,3 +34,5 @@ void output(int current_data[ROWS][COLLS]) {
         }
     }
 }
+
+
